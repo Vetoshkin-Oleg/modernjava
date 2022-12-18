a74b58e7ad7a1455535049225b7750055e86169a
Vetoshkin-Oleg <voa2014@google.com>
2022-12-18 17:59:01 +0500

    Chapter 10: Domain-Specific Languages and Lambda Expressions

---
+++ src/main/java/modernjava/chap10/PrintNumbers.java
@@ -0,0 +1,22 @@
+package modernjava.chap10;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class PrintNumbers {
+
+    public static void main(String[] args) {
+        List<String> numbers = Arrays.asList("one", "two", "three");
+
+        System.out.println("Anonymous class:");
+        numbers.forEach(System.out::println);
+
+        System.out.println("Lambda expression:");
+        numbers.forEach(System.out::println);
+
+        System.out.println("Method reference:");
+        numbers.forEach(System.out::println);
+    }
+
+}
