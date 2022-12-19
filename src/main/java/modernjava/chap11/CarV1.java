40ed6445a6c63482e5c1bf4da1eb97328a148492
Vetoshkin-Oleg <voa2014@google.com>
2022-12-19 21:40:22 +0500

    Chapter 11: Using Optional as a better alternative to null

---
+++ src/main/java/modernjava/chap11/CarV1.java
@@ -0,0 +1,11 @@
+package modernjava.chap11;
+
+public class CarV1 {
+
+    private Insurance insurance;
+
+    public Insurance getInsurance() {
+        return insurance;
+    }
+
+}
---
+++ src/main/java/modernjava/chap11/PersonV1.java
@@ -0,0 +1,11 @@
+package modernjava.chap11;
+
+public class PersonV1 {
+
+    private CarV1 car;
+
+    public CarV1 getCar() {
+        return car;
+    }
+
+}
